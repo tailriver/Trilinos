ee06b77b49eca3cc98e2a42d66a5f0271bc62ee0
Shinsuke Ogawa <ogawa@advancesoft.jp>
2017-05-16 16:08:04 +0900

    Update Version.cmake

--- Version.cmake
+++ Version.cmake
@@ -63,10 +63,10 @@ SET(Trilinos_VERSION 12.11)
 SET(Trilinos_MAJOR_VERSION 12)
 SET(Trilinos_MAJOR_MINOR_VERSION 121100)
 SET(Trilinos_VERSION_STRING "12.11 (Dev)")
-SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
+SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
-SET(Trilinos_REPOSITORY_BRANCH "develop" CACHE INTERNAL "")
+SET(Trilinos_REPOSITORY_BRANCH "advance" CACHE INTERNAL "")
 SET(Trilinos_EXTRAREPOS_BRANCH "master" CACHE INTERNAL "")
 SET(Trilinos_TESTING_TRACK "" CACHE INTERNAL "")
 
